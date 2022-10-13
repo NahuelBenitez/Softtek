3a05c5be2412ba994a8b6fe6ea86c7d881e36bdf
nahuelbenitez <jorgenahuelbenitez07@gmail.com>
2022-10-13 10:25:19 -0300

    Create Login Component

---
+++ src/components/Login.js
@@ -0,0 +1,22 @@
+function Login(){
+return(
+    <>
+    <h2>Formulario de Login</h2>
+    <form>
+        <label>
+            <span>Correo Electronico</span><br />
+        <input type="email" name="email" />
+        </label>
+        <br />
+        <label>
+            <span>Contraseña</span><br />
+            <input type="password" name="password" />
+        </label>
+        
+        <br />
+        <button type="submit">Ingresar</button>
+    </form>
+    </>
+)
+}
+export default Login;
