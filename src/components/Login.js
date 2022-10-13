804307d75050586a9e38ab0a1337e44a11d8d28e
nahuelbenitez <jorgenahuelbenitez07@gmail.com>
2022-10-13 10:55:43 -0300

    Validate email and password in form login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,45 @@
-function Login(){
-return(
+function Login() {
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (email === "" || password === "") {
+      console.log("los campos no pueden estar vacios");
+      return;
+    }
+    if (email !== "" && !regexEmail.test(email)) {
+      console.log("Debes Escribir una direccion de correo electronico valida");
+      return;
+    }
+    if (email !== "challenge@alkemy.org" || password !== "react") {
+      console.log("credenciales invalidas");
+    }
+    console.log("ingresamos al sistema");
+  };
+
+  return (
     <>
-    <h2>Formulario de Login</h2>
-    <form>
+      <h2>Formulario de Login</h2>
+      <form onSubmit={submitHandler}>
         <label>
-            <span>Correo Electronico</span><br />
-        <input type="email" name="email" />
+          <span>Correo Electronico</span>
+          <br />
+          <input type="email" name="email" />
         </label>
         <br />
         <label>
-            <span>Contraseña</span><br />
-            <input type="password" name="password" />
+          <span>Contraseña</span>
+          <br />
+          <input type="password" name="password" />
         </label>
         <br />
         <button type="submit">Ingresar</button>
-    </form>
+      </form>
     </>
-)
+  );
 }
 export default Login;
